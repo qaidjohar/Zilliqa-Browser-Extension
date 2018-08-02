076cb7eb35a9bcf6de26e8f62eb6ea0eaa6f5a70
Qaidjohar <qaidjawadwala@gmail.com>
2018-08-02 18:53:21 +0530

    Added UI for Login Page Draft-1

--- popup.js
+++ popup.js
@@ -1,1 +1,23 @@
+/*function myFunction() {
+    console.log("Function Called");
+    var x = document.getElementById("sec1");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}*/
+
+$(function(){
+    
+    $('#button1').click(function(){
+        $("#login").show();
+        $("#sec1").hide();
+        $("#sec2").hide();
+    })
+    $('#button2').click(function(){
+        $("#sec1").show();
+        $("#sec2").hide();
+    })
+});
 
