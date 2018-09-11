eb599df0bf938693e63a0e6e2232f686f6d34b9c
PATIL Kailas <patilkr24@gmail.com>
2018-09-11 17:32:38 +0530

    "real-time Statistics is added using CoinMarketCap APIs"

--- popup.js
+++ popup.js
@@ -15,6 +15,46 @@ function hideall(){
     $("#sec2").hide();
 }
 
+function formattedDate(unixTimeStamp) {
+	//var unixTimeStamp = 983112343;
+	var timestampInMilliSeconds = unixTimeStamp*1000;
+	var date = new Date(timestampInMilliSeconds);
+
+	var day = (date.getDate() < 10 ? '0' : '') + date.getDate();
+	var month = (date.getMonth() < 9 ? '0' : '') + (date.getMonth() + 1);
+	var year = date.getFullYear();
+
+	var hours = ((date.getHours() % 12 || 12) < 10 ? '0' : '') + (date.getHours() % 12 || 12);
+	var minutes = (date.getMinutes() < 10 ? '0' : '') + date.getMinutes();
+	var meridiem = (date.getHours() >= 12) ? 'pm' : 'am';
+
+	var formattedDate = day + '-' + month + '-' + year + ' at ' + hours + ':' + minutes + ' ' + meridiem;
+
+	return(formattedDate);
+}
+
+function ajaxCall(){
+	$.getJSON('https://api.coinmarketcap.com/v2/ticker/2469/?convert=EUR', function(jsondata) {
+		var zilTime = formattedDate(jsondata.data.last_updated);
+		//$( "#div-stats" ).html( "<p><b> Currency Name:</b> " + jsondata.data.name + "<p><b> Last Updated:</b> " +  zilTime  +"</p> <p><b>USD Price $: </b>" +  jsondata.data.quotes.USD.price + "</p> <p><b>EUR Price &euro;: </b>" +  jsondata.data.quotes.EUR.price + "</p>");
+		//$("#tbl-zildata").html("<b> Currency Name:</b> " + jsondata.data.name);
+		$("#tbl-zilTime").html("<b> Last Updated Time:</b> " + zilTime);
+		$("#tbl-zilCirSupply").html("<b> Circulating Supply:</b> " + jsondata.data.circulating_supply);
+		$("#tbl-zilTotalSupply").html("<b> Total Supply:</b> " + jsondata.data.total_supply);
+		
+		$("#tbl-zilUSDprice").html("<b> USD Price:</b> $" + jsondata.data.quotes.USD.price);
+		$("#tbl-zilUSD24hVol").html("<b> Volume 24hrs:</b> $" + jsondata.data.quotes.USD.volume_24h);
+		$("#tbl-zilUSDmktCap").html("<b> Market Cap:</b> $" + jsondata.data.quotes.USD.market_cap);
+		$("#tbl-zilUSD1hChng").html("<b> Percent Change in 1Hr:</b> $" + jsondata.data.quotes.USD.percent_change_1h);
+		
+		$("#tbl-zilEURprice").html("<b> EUR Price:</b> &euro;" + jsondata.data.quotes.EUR.price);
+		$("#tbl-zilEUR24hVol").html("<b> Volume 24hrs:</b> $" + jsondata.data.quotes.EUR.volume_24h);
+		$("#tbl-zilEURmktCap").html("<b> Market Cap:</b> $" + jsondata.data.quotes.EUR.market_cap);
+		$("#tbl-zilEUR1hChng").html("<b> Percent Change in 1Hr:</b> $" + jsondata.data.quotes.EUR.percent_change_1h);
+   // alert(jsondata.data.name);
+});
+}
+
 $(function(){
     
     /*$('#button1').click(function(){
@@ -38,6 +78,10 @@ $(function(){
     $('#restore-ok').click(function(){
         hideall();
         $("#login").show();
+    });    
+    $('#zil-stats').click(function(){
+       ajaxCall();        
     });
+    
 });
 
